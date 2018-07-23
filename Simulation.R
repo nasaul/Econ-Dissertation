2dc9ba38f411e601f10a2c941c621e3ba7a63f9f
Saul Caballero <saul.caballero.ramirez@gmail.com>
2018-07-23 13:51:47 -0500

    "Added multiprocessor for simulation"

--- Simulation.R
+++ Simulation.R
@@ -9,8 +9,8 @@ library(furrr)
 source("./Functions/Simulation/simulation_function.R")
 source("./Functions/penalized_gmm_functions.R")
 set.seed(123456)
-
+plan(multiprocess)
 sim_1000 <- furrr::future_map(
   rep(0.2, 1000),
-  ~simulation(cl = ., lambdas = c(0, , by = ), eps = 1e-8, N = 500, pi1 = 0.2)
+  ~simulation(cl = ., lambdas = seq(0, 0.2, length.out = 8), eps = 1e-8, N = 500, pi1 = 0.2)
 )
