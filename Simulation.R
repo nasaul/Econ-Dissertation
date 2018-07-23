a7c2084eea6945cfe3ab706152538ed02a673dc6
Saul Caballero <saul.caballero.ramirez@gmail.com>
2018-07-23 16:40:56 -0500

    Lambda value updated

--- Simulation.R
+++ Simulation.R
@@ -12,7 +12,7 @@ set.seed(123456)
 plan(multiprocess)
 sim_500_2 <- furrr::future_map(
   rep(0.2, 1000),
-  ~simulation(cl = ., lambdas = seq(0, 0.2, length.out = 2), eps = 1e-8, N = 500, pi1 = 0.2)
+  ~simulation(cl = ., lambdas = seq(0.0003030303, 0.01, by = 0.0003030303), eps = 1e-8, N = 500, pi1 = 0.2)
 )
 
 saveRDS(sim_500_2, file = "Results/sim_500_2.rds")
@@ -20,7 +20,7 @@ rm(sim_500_2)
 gc()
 sim_1000_2  <- furrr::future_map(
   rep(0.2, 1000),
-  ~simulation(cl = ., lambdas = seq(0, 0.2, length.out = 2), eps = 1e-8, N = 1000, pi1 = 0.2)
+  ~simulation(cl = ., lambdas = seq(0.0003030303, 0.01, by = 0.0003030303), eps = 1e-8, N = 1000, pi1 = 0.2)
 )
 
 saveRDS(sim_1000_2 , file = "Results/sim_1000_2.rds")
