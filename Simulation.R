0e090fb4c8ab1ab2f45905906f07bfb21454d11b
Saul Caballero <saul.caballero.ramirez@gmail.com>
2018-07-23 13:59:30 -0500

    First replications

--- Simulation.R
+++ Simulation.R
@@ -10,7 +10,17 @@ source("./Functions/Simulation/simulation_function.R")
 source("./Functions/penalized_gmm_functions.R")
 set.seed(123456)
 plan(multiprocess)
-sim_1000 <- furrr::future_map(
+sim_500_2 <- furrr::future_map(
   rep(0.2, 1000),
-  ~simulation(cl = ., lambdas = seq(0, 0.2, length.out = 8), eps = 1e-8, N = 500, pi1 = 0.2)
+  ~simulation(cl = ., lambdas = seq(0, 0.2, length.out = 2), eps = 1e-8, N = 500, pi1 = 0.2)
 )
+
+saveRDS(sim_500_2, file = "Results/sim_500_2.rds")
+rm(sim_500_2)
+gc()
+sim_1000_2  <- furrr::future_map(
+  rep(0.2, 1000),
+  ~simulation(cl = ., lambdas = seq(0, 0.2, length.out = 2), eps = 1e-8, N = 1000, pi1 = 0.2)
+)
+
+saveRDS(sim_1000_2 , file = "Results/sim_1000_2.rds")
